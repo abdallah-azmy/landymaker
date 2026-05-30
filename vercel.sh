1fce3647222b122c4db31c59303b198adc3120cc
abdallah-azmy <58990918+abdallah-azmy@users.noreply.github.com>
2026-05-30 15:35:22 +0300

    new deploy (logo)

--- vercel.sh
+++ vercel.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
+
 if [ ! -d "flutter" ]; then
-  git clone -b stable https://github.com --depth 1
+  git clone -b stable https://github.com/flutter/flutter.git --depth 1
 fi
-./flutter/bin/flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+
+export PATH="$PATH:$(pwd)/flutter/bin"
+flutter config --enable-web
+flutter pub get
+flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+
+# Vercel expects output in 'public' directory
+rm -rf public
+mkdir public
+cp -r build/web/* public/

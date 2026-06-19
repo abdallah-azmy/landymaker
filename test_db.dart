41fd72b29e0a10c989f58a4fdd15db494e6be4e5
abdallah-azmy <58990918+abdallah-azmy@users.noreply.github.com>
2026-06-19 14:36:29 +0300

    chore: remove unused static analysis and test scripts

--- test_db.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:supabase/supabase.dart';
-
-void main() async {
-  final supabaseUrl = Platform.environment['SUPABASE_URL'] ?? 'https://npsryevgqghkbllyszet.supabase.co';
-  final supabaseKey = Platform.environment['SUPABASE_ANON_KEY'] ?? 'fake_key';
-}
